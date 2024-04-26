e6d841a35fc500b70e71681995eb4e978e93925f
OlenaYurchuk <145892734+OlenaYurchuk@users.noreply.github.com>
2024-04-26 11:43:44 +0300

    added redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,36 @@
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+}
+
+const persistedContactReducer = persistReducer(persistConfig, contactsReducer);
+
 const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactReducer,
     filters: filtersReducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware({
+    serializableCheck: {
+      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
 });
 
 export default store;
+export const persistor = persistStore(store);
