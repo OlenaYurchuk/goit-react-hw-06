195cfa04b4350ba82b03c96ae1f2d78fb9d809a0
OlenaYurchuk <145892734+OlenaYurchuk@users.noreply.github.com>
2024-04-29 11:30:48 +0300

    updated contactList component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,30 +4,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contactsSlice';
 import css from './ContactList.module.css';
 
+const useFilteredContacts = () => {
+  const contacts = useSelector((state) => state.contacts.items);
+  const filterName = useSelector((state) => state.filters.name.toLowerCase().trim());
+
+  const filteredContacts = useMemo(() => {
+    return contacts.filter((item) => item.name.toLowerCase().trim().includes(filterName));
+  }, [contacts, filterName]);
+
+  return filteredContacts;
+}
+
 export default function ContactList() {
   const dispatch = useDispatch();
-  const useFilteredContacts = () => {
-    const contacts = useSelector(state => state.contacts.items);
-    const filterName = useSelector(state => state.filters.name.toLowerCase().trim());
-
-    const filteredContacts = useMemo(() => {
-      return contacts.filter(item =>
-        item.name.toLowerCase().trim().includes(filterName)
-      );
-    }, [contacts, filterName]);
-
-    return filteredContacts;
-  };
-
-
-  const contacts = useFilteredContacts();
+  const filteredContacts = useFilteredContacts();
 
   const handleDelete = (contactId) => {
     dispatch(deleteContact(contactId));
   }
   return (
     <ul className={css.list}>
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <li key={contact.id}>
           <Contact contact={contact} onDelete={handleDelete} />
         </li>
